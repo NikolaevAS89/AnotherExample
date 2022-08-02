2336a320593326bbd3f2ae881e75e4092766beac
MadRick <t.i.m.e.s.t.o.p@mail.ru>
2022-08-02 21:34:01 +0400

    kotlin test

--- app/src/main/java/com/example/anotherexample/MainActivity.kt
+++ app/src/main/java/com/example/anotherexample/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.anotherexample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.anotherexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val bttnAction: Button = activityMainBinding.bttnAction
+        bttnAction.setOnClickListener { onClick() }
+    }
+
+    private fun onClick() {
+        val viewText: TextView = activityMainBinding.textView
+        viewText.text = "UUUUUrrrr";
     }
 }
